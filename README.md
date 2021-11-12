cf49c431cb02ff4c024a73a42c546993005d0838
boymelvs <ambo_dacoycoy@yahoo.com>
2021-11-12 16:09:55 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Frontend Mentor - INTRO COMPONENT WITH SIGNUP FORM SOLUTION
+
+## Table of contents
+
+-  [Overview](#overview)
+   -  [The challenge](#the-challenge)
+   -  [Screenshot](#screenshot)
+   -  [Links](#links)
+   -  [Built with](#built-with)
+-  [Author](#author)
+
+## Welcome! 👋
+
+Thanks for checking out this front-end coding challenge.
+
+## Overview
+
+This is a solution to the [Intro component with sign up form challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/intro-component-with-signup-form-5cf91bd49edda32581d28fd1).
+
+### The challenge
+
+Users should be able to:
+
+-  View the optimal layout for the site depending on their device's screen size
+-  See hover states for all interactive elements on the page
+-  Receive an error message when the `form` is submitted if:
+   -  Any `input` field is empty. The message for this error should say _"[Field Name] cannot be empty"_
+   -  The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say _"Looks like this is not an email"_
+
+### Screenshot
+
+![Intro component with sign up form solution](./design/desktop-design.png)
+
+### Links
+
+-  Live Site URL: [Live site](https://boymelvs.github.io/INTRO-COMPONENT-WITH-SIGNUP-FORM/)
+
+### Built with
+
+-  HTML
+-  Flexbox
+-  CSS
+-  Javascript
+
+## Author
+
+-  Frontend Mentor - [Melvin](https://www.frontendmentor.io/profile/boymelvs)
---
+++ scripts/index.js
@@ -0,0 +1,71 @@
+const formElement = document.querySelector(".forms");
+const firstNameElement = document.querySelector(".first_name");
+const lastNameElement = document.querySelector(".last_name");
+const emailElement = document.querySelector(".email");
+const passwordElement = document.querySelector(".password");
+
+const btn = document.querySelector(".btn");
+
+/* ================= utility functions start here =================*/
+// check input if empty
+const isRequired = (value) => {
+   return value === "" ? false : true;
+};
+
+// check email if valid
+const isEmailValid = (email) => {
+   const format = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/;
+
+   return format.test(email);
+};
+
+// check password if valid
+const isPasswordSecure = (password) => {
+   const format = new RegExp("^(?=.*[a-zA-Z0-9])(?=.{8,})");
+
+   return format.test(password);
+};
+
+// add or remove class
+const addRemoveClass = (isError, elem) => {
+   const formField = elem.parentElement;
+   const warning = formField.querySelector(".warning p");
+
+   if (isError) {
+      warning.classList.add("message");
+      elem.classList.add("icon_error");
+   } else {
+      warning.classList.remove("message");
+      elem.classList.remove("icon_error");
+   }
+};
+
+/* ================= validating functions start here =================*/
+const checkTheName = (elem) => {
+   const value = elem.value.trim();
+
+   addRemoveClass(!isRequired(value), elem);
+};
+
+const checkEmail = (elem) => {
+   const value = elem.value.trim();
+
+   addRemoveClass(!isRequired(value), elem);
+   addRemoveClass(!isEmailValid(value), elem);
+};
+
+const checkPassword = (elem) => {
+   const value = elem.value.trim();
+
+   addRemoveClass(!isRequired(value), elem);
+   addRemoveClass(!isPasswordSecure(value), elem);
+};
+
+/* ================= claim button here =================*/
+btn.addEventListener("click", (event) => {
+   event.preventDefault();
+   let isFirstNameValid = checkTheName(firstNameElement),
+      isLastNameValid = checkTheName(lastNameElement),
+      isEmailValid = checkEmail(emailElement),
+      isPasswordValid = checkPassword(passwordElement);
+});
